8e9f9d5c5c15fb6b2e011de6fa1f9b2d425d95ef
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2021-07-03 10:47:49 -0300

    Nome de atributo alterado, fotoPerfil para photo

--- lib/data/dummy_users.dart
+++ lib/data/dummy_users.dart
@@ -2,15 +2,9 @@ import 'package:cadastro_usuario/models/user.dart';
 
 const DUMMY_USERS = {
   '1': const User(
-      id: '1',
-      name: 'Eduardo',
-      email: 'eduardof013@hotmail.com',
-      fotoPerfil: "url"),
+      id: '1', name: 'Eduardo', email: 'eduardof013@hotmail.com', photo: "url"),
   '2': const User(
-      id: '1',
-      name: 'Gabriela',
-      email: 'gabriela@hotmail.com',
-      fotoPerfil: "url"),
+      id: '1', name: 'Gabriela', email: 'gabriela@hotmail.com', photo: "url"),
   '3': const User(
-      id: '1', name: 'Maria', email: 'maria@hotmail.com', fotoPerfil: "url"),
+      id: '1', name: 'Maria', email: 'maria@hotmail.com', photo: "url"),
 };
--- lib/models/user.dart
+++ lib/models/user.dart
@@ -4,12 +4,12 @@ class User {
   final String id;
   final String name;
   final String email;
-  final String fotoPerfil;
+  final String photo;
 
   const User({
     this.id,
     @required this.name,
     @required this.email,
-    @required this.fotoPerfil,
+    @required this.photo,
   });
 }
