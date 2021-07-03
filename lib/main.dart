192ed3ef3514b57e404ac4e2ac327a44f1ef981c
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2021-07-03 11:51:30 -0300

    Provider implementado

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,25 @@
+import 'package:cadastro_usuario/provider/users.dart';
 import 'package:cadastro_usuario/views/user_list.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: UserList(),
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ct) => UserProvider(),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Cadastro de Usuários',
+        home: UserList(),
+      ),
+    );
+  }
 }
