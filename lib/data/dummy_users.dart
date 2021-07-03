3e52c9b7b11fdc57c2e8b0864c923ebf3f12afc4
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2021-07-03 09:49:13 -0300

    Mapa de dados criado

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,16 @@
+import 'package:cadastro_usuario/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'Eduardo',
+      email: 'eduardof013@hotmail.com',
+      fotoPerfil: "url"),
+  '2': const User(
+      id: '1',
+      name: 'Gabriela',
+      email: 'gabriela@hotmail.com',
+      fotoPerfil: "url"),
+  '3': const User(
+      id: '1', name: 'Maria', email: 'maria@hotmail.com', fotoPerfil: "url"),
+};
