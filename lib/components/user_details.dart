886e9b2acec460f00d93a611c2e10a8d615a24f5
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2021-07-03 10:42:27 -0300

    Implementado component user details para visualizacao de detalhes dos usuarios

--- lib/components/user_details.dart
+++ lib/components/user_details.dart
@@ -1,8 +1,13 @@
+import 'package:cadastro_usuario/models/user.dart';
 import 'package:flutter/material.dart';
 
 class UserDetails extends StatelessWidget {
+  final User user;
+
+  const UserDetails(this.user);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListTile();
   }
 }
