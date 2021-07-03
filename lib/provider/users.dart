633e2bb04fc4df4e1d6d98fe33502f2f3d0f73c8
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2021-07-03 13:58:08 -0300

    Metodos create, update e read criado

--- lib/provider/users.dart
+++ lib/provider/users.dart
@@ -1,9 +1,36 @@
+import 'dart:math';
+
 import 'package:cadastro_usuario/data/dummy_users.dart';
 import 'package:cadastro_usuario/models/user.dart';
 import 'package:flutter/material.dart';
 
 class UserProvider with ChangeNotifier {
   final Map<String, User> _itens = {...DUMMY_USERS};
+
+  //CREATE
+  void createUser(User user) {
+    if (user == null) {
+      return;
+    }
+
+    final id = Random().nextDouble().toString();
+    _itens.putIfAbsent(
+        id,
+        () => User(
+            id: id, name: user.name, email: user.email, photo: user.photo));
+    notifyListeners();
+  }
+
+// UPDATE
+  void updateUser(User user) {
+    if (user.id != null &&
+        user.id.trim().isNotEmpty &&
+        _itens.containsKey(user.id)) {
+      _itens.update(user.id,
+          (_) => User(name: user.name, email: user.email, photo: user.photo));
+    }
+    notifyListeners();
+  }
 
   // FIND ALL
   List<User> get all {
@@ -14,4 +41,7 @@ class UserProvider with ChangeNotifier {
   int get count {
     return _itens.length;
   }
+
+  // DELETE
+
 }
