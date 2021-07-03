c6a719ba6023fe6ee4f2472da3ef83e36eba415f
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2021-07-03 11:14:13 -0300

    Adicionado mais um usuario para teste

--- lib/data/dummy_users.dart
+++ lib/data/dummy_users.dart
@@ -4,19 +4,25 @@ const DUMMY_USERS = {
   '1': const User(
       id: '1',
       name: 'Eduardo',
-      email: 'eduardof013@hotmail.com',
+      email: 'eduardo@hotmail.com',
       photo:
           "https://cdn.pixabay.com/photo/2016/03/09/15/10/man-1246508_960_720.jpg"),
   '2': const User(
-      id: '1',
+      id: '2',
       name: 'Gabriela',
       email: 'gabriela@hotmail.com',
       photo:
           "https://cdn.pixabay.com/photo/2017/08/23/22/53/portrait-2674726_960_720.jpg"),
   '3': const User(
-      id: '1',
+      id: '3',
       name: 'Maria',
       email: 'maria@hotmail.com',
       photo:
           "https://cdn.pixabay.com/photo/2015/03/04/19/41/woman-659348_960_720.jpg"),
+  '4': const User(
+      id: '4',
+      name: 'David',
+      email: 'david@hotmail.com',
+      photo:
+          "https://cdn.pixabay.com/photo/2017/11/02/14/36/model-2911363_960_720.jpg"),
 };
