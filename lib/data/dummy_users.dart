a05c8a54c1e778c1cb1db7587c1533949d5c6a0d
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2021-07-03 11:05:53 -0300

    Adicionado endereco de foto para usuarios

--- lib/data/dummy_users.dart
+++ lib/data/dummy_users.dart
@@ -2,9 +2,21 @@ import 'package:cadastro_usuario/models/user.dart';
 
 const DUMMY_USERS = {
   '1': const User(
-      id: '1', name: 'Eduardo', email: 'eduardof013@hotmail.com', photo: "url"),
+      id: '1',
+      name: 'Eduardo',
+      email: 'eduardof013@hotmail.com',
+      photo:
+          "https://cdn.pixabay.com/photo/2016/03/09/15/10/man-1246508_960_720.jpg"),
   '2': const User(
-      id: '1', name: 'Gabriela', email: 'gabriela@hotmail.com', photo: "url"),
+      id: '1',
+      name: 'Gabriela',
+      email: 'gabriela@hotmail.com',
+      photo:
+          "https://cdn.pixabay.com/photo/2017/08/23/22/53/portrait-2674726_960_720.jpg"),
   '3': const User(
-      id: '1', name: 'Maria', email: 'maria@hotmail.com', photo: "url"),
+      id: '1',
+      name: 'Maria',
+      email: 'maria@hotmail.com',
+      photo:
+          "https://cdn.pixabay.com/photo/2015/03/04/19/41/woman-659348_960_720.jpg"),
 };
